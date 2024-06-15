d655f6bbe1219ac1f3722e639a15389d89645bad
JuliDell <julianadell_aglio@hotmail.com>
2024-06-15 21:03:58 +0000

    correct spelling errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,27 +4,26 @@ function Quiz(questions) {
     this.currentQuestionIndex= 0;
 }
 
-Quiz.prototype.option = function (answer) {
-    if (this.getCurrentQuestion(). is CoreectAnswer(asnwer)){
+Quiz.prototype.guess = function(answer) {
+    if (this.getCurrentQuestion(). is CorrectAnswer(answer)){
         this.score++;
     }
     this.currentQuestionIndex++;
 };
 Quiz.prototype.getCurrentQuestion= function() {
-    return this.questions [this.currentQuestionIndex]
+    return this.questions [this.currentQuestionIndex];
 };
-Quiz prototype. hasEnded = function () {
-    return this. currentQuestionIndex >= this.questions.lengh;
-}
-function Questions (text, choices, answer) {
+Quiz prototype. hasEnded = function() {
+    return this. currentQuestionIndex >= this.questions.length;
+};
+function Question (text, choices, answer) {
     this.text=text;
     this.choices=choices;
     this.answer=answer;
-}
-Questions.prototype.iscorrectAnswer= function (choice) {
+};
+Questions.prototype.iscorrectAnswer= function(choice) {
     return this.answer===choice;
-
-}
+};
 Var QuizUI= {
     displayNext: function() {
         if (Quiz.hasEnded()) {
@@ -32,45 +31,47 @@ Var QuizUI= {
         } else {
         this.displayQuestion();
         this.displayChoices();
-        this.displayProgress()
+        this.displayProgress();
     }
 },
 displayQuestions: function() {
-    this.populateIdwithHTML ("question", quiz.getCurrentQuestion().text);
-}
+    this.populateIdWithHTML ("question", quiz.getCurrentQuestion().text);
+},
 displayChoices: function() {
     var choices= quiz.getCurrentQuestion().choices;
-}
-    for (vari =0; i < choices.lengh; i++) {
-    this. populateIdwithHTML ("choice" + i, choices [i]);
-    this.guessHandLer ("guess" + i, choices [i]);
+
+    for (var i = 0; i < choices.lengh; i++) {
+    this. populateIdWithHTML ("choice" + i, choices [i]);
+    this.guessHandler ("guess" + i, choices [i]);
     }
+},
 displayScore: function() {
     var gameOverHTML= "<h1>Game Over</h1>";
-    gameOverHTML+="<h2> Your score is: "+ quiz.score + "/5 </h2>"
-    this.populateIdwithHTML ("quiz, gameOverHTML")
-}
-populateIdwithHTML: function (id, text) {
+    gameOverHTML+="<h2> Your score is: "+ quiz.score + "/5 </h2>";
+    this.populateIdWithHTML ("quiz, gameOverHTML");
+},
+populateIdWithHTML: function(id, text) {
     var element= document.getElementById(id);
     element.innerHTML = text;
-}
-guessHandLer: function (id, guess) {
+},
+guessHandler: function(id, guess) {
     var button = document.getElementById(id);
-    button.onclick= function () {
+    button.onclick= function() {
         quiz.guess (guess);
         QuizUI.displayNext();
     }
-}
+},
 displayProgress: function() {
     var currentQuestionNumber= quiz.currentQuestionIndex + 1;
-    this.populateIdwithHTML ("progress", "Question"+ currentQuestionNumber + "of" + quiz.questions.lengh);
-}
+    this.populateIdWithHTML ("progress", "Question"+ currentQuestionNumber + "of" + quiz.questions.length);
+},
+
 var questions = [
-    new Question ("What is meant by one light year?"), ["The distance light travels in one year", "Distance that light travels in one day", "The distance that travels in one min"], "The distance light travels in one year"),
+    new Question ("What is meant by one light year?", ["The distance light travels in one year", "Distance that light travels in one day", "The distance that travels in one min"], "The distance light travels in one year"),
     new Question ("Which is the closest planet to the sun?"), ["Mercure", "Venus", "Mars"],"Mercure"),
-    new Question ("How many minutes does the sun light takes to come to earth?"), ["Just over eight minutes", "Just over four minutes","Just over nine minutes"], "Just over eight minutes"),
-    new Question ("Which is the spacecraft that NASA launched in 2006, to study Pluto?), ["Voyager", "New Horizons", "Messenger"], "New Horizons"),
-    new Question ("Which was the first living creature to be sent to space?"), ["Laika the dog", "Yuri Gagarin", "A monkey"], "Laika the dog"),
+    new Question ("How many minutes does the sun light takes to come to earth?", ["Just over eight minutes", "Just over four minutes","Just over nine minutes"], "Just over eight minutes"),
+    new Question ("Which is the spacecraft that NASA launched in 2006, to study Pluto?, ["Voyager", "New Horizons", "Messenger"], "New Horizons"),
+    new Question ("Which was the first living creature to be sent to space?", ["Laika the dog", "Yuri Gagarin", "A monkey"], "Laika the dog"),
 
 ];
 var quiz= new Quiz(questions);
